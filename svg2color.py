5ce9a50bf92b38d2c3620eabc5c507a5cedfd8e7
Mehmet Selim BILGIN <mselimbilgin@yahoo.com>
2014-10-22 23:27:54 +0300

    v0.9

--- svg2color.py
+++ svg2color.py
@@ -6,8 +6,8 @@
  Generates color-ramp styles from SVG files.  It also compatible with CPT-CITY styles.
  SVG2ColoR improves your color-ramp library, by the way your maps look better.
                               -------------------
-        begin                : 2014-06-17
-		version				 : 0.8
+        begin                : 2014-10-17
+		version				 : 0.9
         copyright            : (C) 2014 by Mehmet Selim BILGIN
         email                : mselimbilgin@yahoo.com
 		web					 : http://cbsuygulama.wordpress.com/svg2color
@@ -33,9 +33,9 @@ from xml.dom import minidom
 import codecs
 import os
 import urllib2
+import tempfile
 
 class SVG2ColoR:
-
 	def __init__(self, iface):
 		self.iface = iface
 
@@ -80,7 +80,7 @@ class SVG2ColoR:
 	def loadFromURL(self):
 		##URl reading.
 		try:
-			urlSVG = urllib2.urlopen(self.dlg.lineEdit.text()).read()
+			urlSVG = urllib2.urlopen(self.dlg.lineEdit.text().encode('utf-8')).read()
 			self.isLinearGrad(urlSVG)
 		except Exception as urlError:
 			QMessageBox.critical(None, "Information", 'Cannot load from the given URL: ' + str(urlError))
@@ -120,6 +120,8 @@ class SVG2ColoR:
 				QMessageBox.critical(None, "Information", 'The SVG file does not contain <b>linearGradient</b> tag. Please choose a proper SVG document. You can find lots of samples in <a href="http://soliton.vm.bytemark.co.uk/pub/cpt-city/">CPT-CITY webpage</a>')
 				self.graphicRenderer(open(os.path.dirname(os.path.realpath(__file__)) + os.sep + 'error.svg', 'r').read())
 				self.dlg.pushButton_2.setEnabled(False)
+				self.dlg.pushButton_3.setEnabled(False)
+				self.dlg.label.setEnabled(False)
 			else:				
 				try:
 					self.colorList = list()
@@ -131,15 +133,22 @@ class SVG2ColoR:
 							self.colorList.append([i.attributes['stop-color'].value[4:-1], float(i.attributes['offset'].value[:-1])/100])
 					self.sampleSvg(self.firstNode[0])
 					self.dlg.pushButton_2.setEnabled(True)
+					self.dlg.pushButton_3.setEnabled(True)
+					self.dlg.label.setEnabled(True)
+					
 				except Exception as linearGradError:
 					QMessageBox.critical(None, "Information", 'Cannot read the color values. Please choose a proper SVG document. You can find lots of samples in <a href="http://soliton.vm.bytemark.co.uk/pub/cpt-city/">CPT-CITY webpage</a>')
 					self.graphicRenderer(open(os.path.dirname(os.path.realpath(__file__)) + os.sep + 'error.svg', 'r').read())
 					self.dlg.pushButton_2.setEnabled(False)
+					self.dlg.pushButton_3.setEnabled(False)
+					self.dlg.label.setEnabled(False)
 					
 		except Exception as xmlError:
 			QMessageBox.critical(None, "Information", 'The SVG file is not valid (XML syntax error).')
 			self.graphicRenderer(open(os.path.dirname(os.path.realpath(__file__)) + os.sep + 'error.svg', 'r').read())
 			self.dlg.pushButton_2.setEnabled(False)
+			self.dlg.pushButton_3.setEnabled(False)
+			self.dlg.label.setEnabled(False)
 			
 	def hexToRgb(self, hexademical): #Thanks for dan_waterworth from http://stackoverflow.com/questions/4296249/how-do-i-convert-a-hex-triplet-to-an-rgb-tuple-and-back
 		num = str(hexademical[1:])
@@ -148,8 +157,6 @@ class SVG2ColoR:
 			num = num[:1]*2 + num[1:2]*2 + num[2:3]*2
 		
 		return (str(int(num[:2], 16)) + ',' + str(int(num[2:4], 16)) + ',' + str(int(num[4:6], 16)))
-		
-
 	
 	def styleMaker(self, colorList, name):
 		##Generating QGIS style XML document.
@@ -194,11 +201,10 @@ class SVG2ColoR:
 		prop4.setAttribute('k', 'stops')
 		prop4.setAttribute('v', ':'.join(stopList))
 		colorRamp.appendChild(prop4)
-		return (styleDoc.toprettyxml(indent="    ", encoding="utf-8"))
-		
+		return (styleDoc.toprettyxml(indent="    ", encoding="utf-8"))		
 		
 	def sampleSvg(self, linearGradientTag):
-		##Generating sample SVG document for visualisation.
+		#Loading sample SVG documents for visualisation.
 		sampleSVG = minidom.Document()
 		svgBase = sampleSVG.createElement('svg')
 		svgBase.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
@@ -228,28 +234,49 @@ class SVG2ColoR:
 		self.dlg.graphicsView.setScene(scene)
 		webview = QGraphicsWebView()
 		webview.setContent(svgInput, 'image/svg+xml')
-		scene.addItem(webview)
-		
+		scene.addItem(webview)		
 		
 	def exportStyle(self):
 		browseDlg = QFileDialog.getSaveFileName(self.dlg, 'Save Style File...', os.path.dirname(self.dlg.lineEdit.text()) + os.sep + os.path.splitext(os.path.basename(self.dlg.lineEdit.text()))[0], 'XML file (*.xml)')
+		styleName = os.path.splitext(os.path.basename(self.dlg.lineEdit.text()))[0] #name of the style
 		if browseDlg:
 			try:
 				with codecs.open(browseDlg, encoding='utf-8', mode='w') as saveFile:
-					saveFile.write(self.styleMaker(self.colorList, os.path.splitext(os.path.basename(self.dlg.lineEdit.text()))[0]))
+					saveFile.write(self.styleMaker(self.colorList, styleName))
 					QMessageBox.information(None, "Information", 'The style file succesfully saved in <b>%s</b>. \nNow you can import it from <b>Settings -> Style Manager</b> menu.' % (saveFile.name))
 
 			except Exception as saveError:
 				QMessageBox.critical(None, "Information", ("An error has occured: " + str(saveError)))
-
+				
+	def importStyle(self):				
+		styleName = os.path.splitext(os.path.basename(self.dlg.lineEdit.text()))[0] #name of the style.
+		tempStyle = self.tempDir + os.sep + styleName + '.xml'
+		
+		try:
+			with codecs.open(tempStyle, encoding='utf-8', mode='w') as saveFile:
+				saveFile.write(self.styleMaker(self.colorList, styleName))
+										
+			styleMan = QgsStyleV2.defaultStyle() #QgsStyleV2 style.
+			styleMan.importXML(tempStyle) #Importing the style file. It does not save. So this handled in below line.
+			styleMan.saveColorRamp(styleName, styleMan.colorRamp(styleName), 7, 'SVG2ColoR') #Saving the imported style.
+			
+			QMessageBox.information(None, "Information", 'The style has been succesfully imported.')
+			
+		except Exception as importError:
+			QMessageBox.critical(None, "Information", ("An error has occured: " + str(importError)))
 
 	def run(self):
 		self.dlg = SVG2ColoRDialog()
+		self.dlg.setFixedSize(560,330) #disables resizing the window.
+		
 		self.optionHandler()
+		self.tempDir = tempfile.mkdtemp('svg2color') #Generating temporary directory for importing styles.
+													 #I didnt use tempfile.TemporaryFile() because i got errors when working on Linux. But works without error on Windows.
 		self.dlg.radioButton.toggled.connect(self.optionHandler)
 		self.dlg.radioButton_2.toggled.connect(self.optionHandler)
 		self.dlg.pushButton_2.clicked.connect(self.exportStyle)
+		self.dlg.pushButton_3.clicked.connect(self.importStyle)
 		self.graphicRenderer(open(os.path.dirname(os.path.realpath(__file__)) + os.sep + 'opening.svg', 'r').read())
-		self.dlg.exec_()
+		self.dlg.show()
 
 
